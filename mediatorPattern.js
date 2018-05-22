759c4ec11d0a15e6c7ecdacbc39d066a8e08f857
Liviu Ioan Iancu <LiviuLvu@users.noreply.github.com>
2018-05-22 22:23:08 +0300

    Mediator Pattern

---
+++ mediatorPattern.js
@@ -0,0 +1,10 @@
+// Mediator Pattern
+// https://addyosmani.com/resources/essentialjsdesignpatterns/book/#mediatorpatternjavascript
+
+
+// Mediator holds logic that coordinates objects actions
+
+// obj1 <--> \
+// obj1 <-->  - Mediator Logic
+// obj1 <--> /
+// objN <--> 
