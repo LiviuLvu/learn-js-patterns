255282ab4d0f0bde0bbc2d978483974a98720e1c
Liviu Ioan Iancu <LiviuLvu@users.noreply.github.com>
2018-05-26 21:20:52 +0300

    Apply eslint recommended fixes

--- singletonPattern.js
+++ singletonPattern.js
@@ -1,31 +1,32 @@
 // Singleton Pattern
 // https://addyosmani.com/resources/essentialjsdesignpatterns/book/#singletonpatternjavascript
 
-// In practice, the Singleton pattern is useful when exactly one object is needed to coordinate others across a system
+// In practice, the Singleton pattern is useful when exactly
+// one object is needed to coordinate others across a system
 // Returns a structure
 // Is a module pattern that allows the possibility of instantiating only one object
 
-let aSingleton = (() => {
+const aSingleton = (() => {
   let instance;
-  
-  let init = () => {
-    let privateVariable = 'private string';
-    let privateNumber = Math.random();
+
+  const init = () => {
+    const privateNumber = Math.random();
     return {
       publicVar: 0,
-      publicMethod: privateNumber
-    }
-  }
-  
+      publicMethod: privateNumber,
+    };
+  };
+
   return {
     getInstance: () => {
-      if(instance === undefined) instance = init();
+      if (instance === undefined) instance = init();
       return instance;
-    }
-  }
+    },
+  };
 })();
 
-instance1 = aSingleton.getInstance();
-instance2 = aSingleton.getInstance();
+const instance1 = aSingleton.getInstance();
+const instance2 = aSingleton.getInstance();
 
 console.log('Does instance1 === instance2 ? ', instance1 === instance2);
+console.log('node module object looks like this >>>> ', module);
